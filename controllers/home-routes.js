4c0481eec9ba2bed59cb7b2cc830945d8ece1716
vpratnicki <vanessa@pratnicki.net>
2022-10-29 13:41:25 -0400

    Setup signle post feature

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
+// show all posts on homepage
 router.get('/', (req, res) => {
     console.log(req.session);
     Post.findAll({
@@ -32,13 +33,65 @@ router.get('/', (req, res) => {
         console.log(dbPostData[0]);
         const posts = dbPostData.map(post => post.get({ plain: true }));
 
-        res.render('homepage', { posts });
+        res.render('homepage', { 
+          posts,
+          loggedIn: req.session.loggedIn
+         });
       })
       .catch(err => {
         console.log(err);
         res.status(500).json(err);
       });
   });
+
+// single post route
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+          id: req.params.id
+        },
+        attributes: [
+          'id',
+          'post_url',
+          'title',
+          'created_at',
+          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+      })
+        .then(dbPostData => {
+          if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+          }
+    
+          // serialize the data
+          const post = dbPostData.get({ plain: true });
+    
+          // pass data to template
+          res.render('single-post', { 
+            post,
+            loggedIn: req.session.loggedIn
+           });
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+});
 
 //login route
 router.get('/login', (req, res) => {
